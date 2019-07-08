76edda11f5c758711115c3fa2514aca7d4ab073d
CBMC Ukraine <github@cbmcukraine.com>
2019-07-08 09:43:45 -0700

    fixed length

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,6 +7,20 @@ import verticalCBMC from '../img/verticalCBMC.svg'
 const url = `/img/ukraine_houses.jpg`
 
 const Card = ({ title, image }) => {
+
+  let words = []
+  const array = title.split(' ')
+  array.map( (word, index) => {
+    if(word.length > 3){
+      words.push(word)
+    }else{
+      let last = words.pop()
+      last = last + ' ' + word
+      words.push(last)
+    }
+
+  })
+
   return (
     <div
       className="card full-width-image margin-top-0"
@@ -24,7 +38,7 @@ const Card = ({ title, image }) => {
           }}
         >
           {
-            title.split(' ').map( (word, key) => <p key={key}>{word}</p>)
+            words.map( (word, key) => <p key={key}>{word}</p>)
           }
         </h1>
       </div>
