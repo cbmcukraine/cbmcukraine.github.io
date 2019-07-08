10c22bd4a8bdd275df3664104153362b2d19c57c
CBMC Ukraine <github@cbmcukraine.com>
2019-07-08 06:05:29 -0700

    fix

--- src/pages/contact/thanks.js
+++ src/pages/contact/thanks.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import Layout from '../../components/Layout'
 
-export default () => (
-  <Layout>
+export default ({pageContext}) => (
+  <Layout pageContext={pageContext}>
     <section className="section">
       <div className="container">
         <div className="content">
